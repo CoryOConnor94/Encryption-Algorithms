2b90e63eb3aa1e5314ccd8cd1ae5ee2e50904a51
Cory <CoryOConnor1994@gmail.com>
2024-09-20 12:36:24 +0100

    Update des.py
    Added encrypt and decrypt functions and takes message as input from user

--- des.py
+++ des.py
@@ -6,27 +6,29 @@ from Crypto.Random import get_random_bytes
 
 # Size of private key must be 64 bits (8 bytes)
 # User input can be hashed using SHA to transform key to 64 bits
-
 key = b'mysecret'
-
-# Create cipher object for encryption with private key and IV method
 encryption_cipher = DES.new(key, DES.MODE_CBC)
-# Print IV of object
-print(encryption_cipher.IV)
-# Print Block size
-print(encryption_cipher.block_size)
 
-plaintext = b'This is a message'
-# Pad plaintext to fit block size and encrypt
-cipher_text = encryption_cipher.encrypt(pad(plaintext, DES.block_size))
-# Display cipher text in ascii format rather than bytes
-print(binascii.hexlify(cipher_text))
+def des_encrypt(plain_text):
+    # Pad plaintext to fit block size and encrypt
+    cipher_text = encryption_cipher.encrypt(pad(plain_text, DES.block_size))
+    # Return cipher text in ascii format rather than bytes
+    return cipher_text
 
 
-# Store iv used in encryption to variable for use in decryption
-iv = encryption_cipher.iv
-# Create cipher object for decryption with key and IV method and iv used in encryption
-decrypt_cipher = DES.new(key, DES.MODE_CBC, iv)
-decrypted_message = decrypt_cipher.decrypt(cipher_text)
-decrypted_message = unpad(decrypted_message, DES.block_size)
+def des_decrypt(cipher_text):
+    # Store iv used in encryption to variable for use in decryption
+    iv = encryption_cipher.iv
+    # Create cipher object for decryption with key and IV method and iv used in encryption
+    decrypt_cipher = DES.new(key, DES.MODE_CBC, iv)
+    decrypted_message = decrypt_cipher.decrypt(cipher_text)
+    decrypted_message = unpad(decrypted_message, DES.block_size)
+    return decrypted_message
+
+
+message = input("Enter message to be encrypted: ")
+bytes_message = message.encode('UTF-8')
+encrypted_message= des_encrypt(bytes_message)
+print(binascii.hexlify(encrypted_message))
+decrypted_message = des_decrypt(encrypted_message)
 print(decrypted_message)
